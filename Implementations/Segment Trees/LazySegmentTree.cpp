8ecf06e0945daf59ae6cca0d4e487c2d15d93e8c
Walden Yan <walnutwaldo20@gmail.com>
2017-08-21 18:59:48 -0400

    Create LazySegmentTree.cpp

---
+++ Implementations/Segment Trees/LazySegmentTree.cpp
@@ -0,0 +1,53 @@
+struct LazySegmentTree {
+
+    int s, e;
+    ll val = 0, lazy = 0;
+    SegmentTree *lChild, *rChild;
+
+    LazySegmentTree(int sz) {
+        this->s = 0;
+        this->e = sz;
+        lChild = NULL, rChild = NULL;
+    }
+
+    LazySegmentTree(int s, int e) {
+        this->s = s;
+        this->e = e;
+        lChild = NULL, rChild = NULL;
+    }
+
+    void update(int l, int r, ll v) {
+        val += (r - l) * v;
+        if(l == s && r == e) {
+            lazy += v;
+            return;
+        }
+        int mid = (s + e) >> 1;
+        if(lazy != 0) {
+            if(lChild == NULL) lChild = new SegmentTree(s, mid);
+            if(rChild == NULL) rChild = new SegmentTree(mid, e);
+            lChild->val += (mid - s) * lazy;
+            rChild->val += (e - mid) * lazy;
+            lChild->lazy += lazy;
+            rChild->lazy += lazy;
+            lazy = 0;
+        }
+        if(l < mid) {
+            if(lChild == NULL) lChild = new SegmentTree(s, mid);
+            lChild->update(l, min(r, mid), v);
+        }
+        if(r > mid) {
+            if(rChild == NULL) rChild = new SegmentTree(mid, e);
+            rChild->update(max(l, mid), r, v);
+        }
+    }
+
+    ll query(int l, int r) {
+        if(l == s && r == e) return val;
+        int mid = (s + e) >> 1;
+        ll res = 0;
+        if(l < mid && lChild != NULL) res += lChild->query(l, min(mid, r));
+        if(r > mid && rChild != NULL) res += rChild->query(max(mid, l), r);
+        return res;
+    }
+};
