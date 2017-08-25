d75b8ebe58bd00b25ec82647e08a28a2470ef267
Walden Yan <walnutwaldo20@gmail.com>
2017-08-25 00:27:38 -0400

    Update E.cpp

--- CodeForces/Education Round 27/E.cpp
+++ CodeForces/Education Round 27/E.cpp
@@ -44,22 +44,25 @@ struct LazySegmentTree {
         this->e = e;
     }
 
+    void propogateLazy() {
+        int mid = (s + e) >> 1;
+        if(lChild == NULL) lChild = new LazySegmentTree(s, mid);
+        if(rChild == NULL) rChild = new LazySegmentTree(mid, e);
+        lChild->val += lazy;
+        rChild->val += lazy;
+        lChild->lazy += lazy;
+        rChild->lazy += lazy;
+        lazy = 0;
+    }
+
     void update(int l, int r, ll v) {
         if(l == s && r == e) {
             val += v;
             lazy += v;
             return;
         }
+        if(lazy != 0) propogateLazy();
         int mid = (s + e) >> 1;
-        if(lazy != 0) {
-            if(lChild == NULL) lChild = new LazySegmentTree(s, mid);
-            if(rChild == NULL) rChild = new LazySegmentTree(mid, e);
-            lChild->val += lazy;
-            rChild->val += lazy;
-            lChild->lazy += lazy;
-            rChild->lazy += lazy;
-            lazy = 0;
-        }
         if(l < mid) {
             if(lChild == NULL) lChild = new LazySegmentTree(s, mid);
             lChild->update(l, min(r, mid), v);
@@ -75,16 +78,8 @@ struct LazySegmentTree {
 
     ll query(int l, int r) {
         if(l == s && r == e) return val;
+        if(lazy != 0) propogateLazy();
         int mid = (s + e) >> 1;
-        if(lazy != 0) {
-            if(lChild == NULL) lChild = new LazySegmentTree(s, mid);
-            if(rChild == NULL) rChild = new LazySegmentTree(mid, e);
-            lChild->val += lazy;
-            rChild->val += lazy;
-            lChild->lazy += lazy;
-            rChild->lazy += lazy;
-            lazy = 0;
-        }
         ll lVal = 0, rVal = 0;
         if(l < mid && lChild != NULL) lVal = lChild->query(l, min(mid, r));
         if(r > mid && rChild != NULL) rVal = rChild->query(max(mid, l), r);
