203a2b1fd165ffe23ea34c5a6f62a85b947806e9
Walden Yan <walnutwaldo20@gmail.com>
2017-08-25 00:05:44 -0400

    Create ImplicitLazyMaxSegmentTree.cpp

---
+++ Implementations/Segment Trees/ImplicitLazyMaxSegmentTree.cpp
@@ -0,0 +1,65 @@
+struct LazySegmentTree {
+
+    int s, e;
+    ll val = 0, lazy = 0;
+    LazySegmentTree *lChild = NULL, *rChild = NULL;
+
+    LazySegmentTree(int sz) {
+        this->s = 0;
+        this->e = sz;
+    }
+
+    LazySegmentTree(int s, int e) {
+        this->s = s;
+        this->e = e;
+    }
+
+    void update(int l, int r, ll v) {
+        if(l == s && r == e) {
+            val += v;
+            lazy += v;
+            return;
+        }
+        int mid = (s + e) >> 1;
+        if(lazy != 0) {
+            if(lChild == NULL) lChild = new LazySegmentTree(s, mid);
+            if(rChild == NULL) rChild = new LazySegmentTree(mid, e);
+            lChild->val += lazy;
+            rChild->val += lazy;
+            lChild->lazy += lazy;
+            rChild->lazy += lazy;
+            lazy = 0;
+        }
+        if(l < mid) {
+            if(lChild == NULL) lChild = new LazySegmentTree(s, mid);
+            lChild->update(l, min(r, mid), v);
+        }
+        if(r > mid) {
+            if(rChild == NULL) rChild = new LazySegmentTree(mid, e);
+            rChild->update(max(l, mid), r, v);
+        }
+        if(lChild == NULL) val = max(0LL, rChild->val);
+        else if(rChild == NULL) val = max(0LL, lChild->val);
+        else val = max(lChild->val, rChild->val);
+    }
+
+    ll query(int l, int r) {
+        if(l == s && r == e) return val;
+        int mid = (s + e) >> 1;
+        if(lazy != 0) {
+            if(lChild == NULL) lChild = new LazySegmentTree(s, mid);
+            if(rChild == NULL) rChild = new LazySegmentTree(mid, e);
+            lChild->val += lazy;
+            rChild->val += lazy;
+            lChild->lazy += lazy;
+            rChild->lazy += lazy;
+            lazy = 0;
+        }
+        ll lVal = 0, rVal = 0;
+        if(l < mid && lChild != NULL) lVal = lChild->query(l, min(mid, r));
+        if(r > mid && rChild != NULL) rVal = rChild->query(max(mid, l), r);
+        if(r <= mid) return lVal;
+        else if(l >= mid) return rVal;
+        else return max(lVal, rVal);
+    }
+};
