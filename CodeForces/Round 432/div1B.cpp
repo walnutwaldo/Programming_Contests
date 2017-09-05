b0f2d7fcaa6539716f6755ccbbe294e235d61a16
Walden Yan <walnutwaldo20@gmail.com>
2017-09-04 20:49:42 -0400

    Non-Cheap Solution

--- CodeForces/Round 432/div1B.cpp
+++ CodeForces/Round 432/div1B.cpp
@@ -1,4 +1,3 @@
-// cheap solution
 #include <bits/stdc++.h>
 
 #define SQ(a) (a)*(a)
@@ -29,14 +28,36 @@ typedef vector<int> vi;
 typedef complex<ld> point;
 
 int n;
-ll x, y;
 bool composite[MAX + 1];
-int occurences[MAX + 1], a[MAXN];
-vector<pii> v;
+ll x, y, sumTo[MAX + 1], prefixCnt[MAX + 1];
+int mostChange;
+
+ll cnt(int l, int r) {
+    if(r < l) return 0;
+    if(r < 0) return 0;
+    if(l > MAX) return 0;
+    r = min(r, MAX);
+    l = min(l, MAX);
+    if(l <= 0) return prefixCnt[r];
+    else return prefixCnt[r] - prefixCnt[l - 1];
+}
+
+ll sum(int l, int r) {
+    if(r < l) return 0;
+    if(r < 0) return 0;
+    if(l > MAX) return 0;
+    r = min(r, MAX);
+    if(l <= 0) return sumTo[r];
+    else return sumTo[r] - sumTo[l - 1];
+}
 
 ll cost(int p) {
     ll total = 0;
-    F0R(i, n) total += min(x, y * (ll)(ceil((double)a[i] / p) * p - a[i]));
+    for(int j = p; j - p + 1 <= MAX; j += p) {
+        int f = max(j - p + 1, j - mostChange);
+        total += x * cnt(j - p + 1, f - 1);
+        total += y * (cnt(f, j) * j - sum(f, j));
+    }
     return total;
 }
 
@@ -45,19 +66,20 @@ int main() {
     cin.tie(0);
     FOR(i, 2, MAX + 1) if(!composite[i]) for(int j = 2 * i; j <= MAX; j += i) composite[j] = true;
     cin >> n >> x >> y;
+    mostChange = x / y;
     F0R(i, n) {
-        cin >> a[i];
-        occurences[a[i]]++;
+        ll a;
+        cin >> a;
+        sumTo[(int)a] += a;
+        prefixCnt[(int)a]++;
     }
-    FOR(i, 2, MAX + 1) if(!composite[i]) {
-        int total = 0;
-        for(int j = i; j <= MAX; j += i) total += occurences[j] + occurences[j - 1];
-        v.PB(MP(total, i));
+    FOR(i, 1, MAX + 1) {
+        sumTo[i] += sumTo[i - 1];
+        prefixCnt[i] += prefixCnt[i - 1];
     }
-    sort(v.rbegin(), v.rend());
     ll ans = -1;
-    F0R(i, 4) {
-        ll temp = cost(v[i].S);
+    FOR(i, 2, MAX + 1) if(!composite[i]) {
+        ll temp = cost(i);
         if(ans == -1 || temp < ans) ans = temp;
     }
     cout << ans << "\n";
