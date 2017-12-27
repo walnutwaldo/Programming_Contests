793cdb8d4a4348b080dbd848fa508c5d6a33b060
Walden Yan <walnutwaldo20@gmail.com>
2017-12-27 18:19:22 -0500

    Update SuffixArray.cpp

--- Implementations/Strings/SuffixArray.cpp
+++ Implementations/Strings/SuffixArray.cpp
@@ -1,24 +1,41 @@
 // Naive Algorithm
 struct SuffixArray {
 
-    vector<pair<string, int>> suffixArray;
+    string s;
+    vi suffixArray;
 
-    SuffixArray(string s) {
-        string temp = "";
-        R0F(i, s.size()) {
-            temp = s[i] + temp;
-            suffixArray.PB(MP(temp, i));
+    bool lessThan(int i, int j) const {
+        F0R(p, s.size() + 1 - max(i, j)) {
+            if(s[i + p] < s[j + p]) return 1;
+            else if(s[i + p] > s[j + p]) return 0;
         }
-        sort(suffixArray.begin(), suffixArray.end());
+        return i > j;
+    }
+
+    void merge_sort(int lo, int hi, vi temp) {
+        if(hi == lo + 1) return;
+        int mid = (lo + hi) >> 1;
+        merge_sort(lo, mid, temp);
+        merge_sort(mid, hi, temp);
+        int i = lo, j = mid;
+        while(i < mid || j < hi) {
+            if(i == mid || j < hi && lessThan(suffixArray[j], suffixArray[i])) temp[i + j - mid] = suffixArray[j++];
+            else temp[i + j - mid] = suffixArray[i++];
+        }
+        FOR(i, lo, hi) suffixArray[i] = temp[i];
+    }
+
+    SuffixArray(string _s) {
+        s = _s;
+        F0R(i, s.size()) suffixArray.PB(i);
+        merge_sort(0, suffixArray.size(), vi(suffixArray.size()));
     }
 
     int lcp(int i, int j) {
         if(i < 0 || i >= suffixArray.size() || j < 0 || j >= suffixArray.size()) return 0;
-        string a = suffixArray[i].F;
-        string b = suffixArray[j].F;
         int res = 0;
-        F0R(p, min(a.size(), b.size())) {
-            if(a[p] == b[p]) res++;
+        F0R(p, s.size() + 1 - max(suffixArray[i], suffixArray[j])) {
+            if(s[suffixArray[i] + p] == s[suffixArray[j] + p]) res++;
             else break;
         }
         return res;
