c25c851ff3b8059ec37e1fe09ab11da935e7c1c1
Walden Yan <walnutwaldo20@gmail.com>
2017-12-27 17:37:14 -0500

    Create SuffixArray.cpp

---
+++ Implementations/Strings/SuffixArray.cpp
@@ -0,0 +1,26 @@
+// Naive Algorithm
+struct SuffixArray {
+
+    vector<pair<string, int>> suffixArray;
+
+    SuffixArray(string s) {
+        string temp = "";
+        R0F(i, s.size()) {
+            temp = s[i] + temp;
+            suffixArray.PB(MP(temp, i));
+        }
+        sort(suffixArray.begin(), suffixArray.end());
+    }
+
+    int lcp(int i, int j) {
+        if(i < 0 || i >= suffixArray.size() || j < 0 || suffixArray.size()) return 0;
+        string a = suffixArray[i].F;
+        string b = suffixArray[j].F;
+        int res = 0;
+        F0R(p, min(a.size(), b.size())) {
+            if(a[p] == b[p]) res++;
+            else break;
+        }
+        return res;
+    }
+};
