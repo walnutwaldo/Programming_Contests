81c46ce7027e0f911de2f5afbf4ba96c408525fc
Walden Yan <walnutwaldo20@gmail.com>
2018-01-07 20:57:52 -0500

    Update Polynomial.cpp

--- Implementations/Data Structures/Math/Polynomial.cpp
+++ Implementations/Data Structures/Math/Polynomial.cpp
@@ -1,4 +1,5 @@
-cld FFTBuild[1 << 19][2], tempPVPoly[1 << 19], rt[1 << 19];
+const int MAX_DEG = 19;
+cld FFTBuild[1 << MAX_DEG][2], tempPVPoly[1 << MAX_DEG], rt[1 << MAX_DEG];
 
 struct Polynomial {
 
@@ -7,7 +8,7 @@ struct Polynomial {
 
     static void buildRT() {
         if(rt[0].R == 1) return;
-        F0R(i, 1 << 19) rt[i] = cld(cos(2 * i * PI / (1 << 19)), sin(2 * i * PI / (1 << 19)));
+        F0R(i, 1 << MAX_DEG) rt[i] = cld(cos(2 * i * PI / (1 << MAX_DEG)), sin(2 * i * PI / (1 << MAX_DEG)));
     }
 
     Polynomial(int d = 0) {
@@ -19,7 +20,7 @@ struct Polynomial {
 
     void FFT(int neededDeg) {
         R0F(i, neededDeg + 1) {
-            int arr = i & 1, narr = arr ^ 1, lli = 1 << i, llil = lli << 1, llndim1 = 1 << (neededDeg - i - 1), rtp = lli << (19 - neededDeg);
+            int arr = i & 1, narr = arr ^ 1, lli = 1 << i, llil = lli << 1, llndim1 = 1 << (neededDeg - i - 1), rtp = lli << (MAX_DEG - neededDeg);
             if(i == neededDeg) {
                 F0R(j, deg + 1) FFTBuild[j][arr] = coefficients[j];
                 FOR(j, deg + 1, lli) FFTBuild[j][arr] = 0;
@@ -39,10 +40,10 @@ struct Polynomial {
     }
 
     static void invFFT(cld *p, int pdeg) {
-        int mp = 1 << 19;
+        int mp = 1 << MAX_DEG;
         cld u = 1 << pdeg;
         R0F(i, pdeg + 1) {
-            int arr = i & 1, narr = arr ^ 1,lli = 1 << i,llil = lli << 1, llpdim1 = 1 << (pdeg - i - 1), rtp = lli << (19 - pdeg);
+            int arr = i & 1, narr = arr ^ 1,lli = 1 << i,llil = lli << 1, llpdim1 = 1 << (pdeg - i - 1), rtp = lli << (MAX_DEG - pdeg);
             R0F(j, lli) {
                 if(i == pdeg) FFTBuild[j][arr] = p[j] / u;
                 else {
