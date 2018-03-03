672bf19ed53f22718a1ab58ab36379abc5700322
Walden Yan <walnutwaldo20@gmail.com>
2018-03-03 01:25:25 -0500

    Update bcs(slow 2).cpp

--- USACO/2016 Season/US Open/Platinum/bcs(slow 2).cpp
+++ USACO/2016 Season/US Open/Platinum/bcs(slow 2).cpp
@@ -1,4 +1,3 @@
-// Too slow
 #include <bits/stdc++.h>
 
 #define SQ(a) (a)*(a)
@@ -8,178 +7,241 @@
 #define R0F(i, a) for(int i = (a) - 1; i >= 0; i--)
 #define ROF(i, a, b) for(int i = (b) - 1; i >= (a); i--)
 
+#define ran() (rand() & 0x7FFF)
+#define rand31() ((ran() << 16) | (ran() << 1) | (ran() & 1))
+#define rand32() ((ran() << 17) | (ran() << 2) | (ran() & 3))
+#define rand63() (((ll)ran() << 48) | ((ll)ran() << 33) | ((ll)ran() << 18) | ((ll)ran() << 3) | ((ll)ran() & 7))
+#define rand64() (((ll)ran() << 49) | ((ll)ran() << 34) | ((ll)ran() << 19) | ((ll)ran() << 4) | ((ll)ran() & 15))
+
 #define F first
 #define S second
-#define A second.first
-#define firstSpot second.second
 #define PB push_back
 #define MP make_pair
 #define MT make_tuple
 #define UB upper_bound
 #define LB lower_bound
-#define fig pair<pair<string*, int>, pair<int, pii>>
-#define MAXN 500
+#define X real()
+#define Y imag()
 #define MAXK 100
+#define MAXN 500
+#define MOD 1000000007
 
 using namespace std;
 
 typedef long long ll;
 typedef long double ld;
 typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 typedef vector<int> vi;
+typedef complex<ld> point;
+
+const pll prime = MP(73, 97);
+pll powPrime[SQ(MAXN) + 1];
+
+pll operator +(pll p1, pll p2) {
+    return MP((p1.F + p2.F) % MOD, (p1.S + p2.S) % MOD);
+}
+
+pll operator -(pll p1, pll p2) {
+    return MP((p1.F + MOD -  p2.F) % MOD, (p1.S + MOD - p2.S) % MOD);
+}
+
+pll operator *(pll p1, pll p2) {
+    return MP((p1.F * p2.F) % MOD, (p1.S * p2.S) % MOD);
+}
+
+pll operator *(pll p, ll l) {
+    return p * MP(l, l);
+}
+
+pii operator -(pii p1, pii p2) {
+    return MP(p1.F - p2.F, p1.S - p2.S);
+}
+
+int n, m;
+
+struct Figure {
+
+    char **color;
+    int **pref;
+    int rows, cols;
+    pll hash = MP(0, 0);
+    pii bottomRight;
+
+    Figure(int rows = 0, int cols = 0) {
+        this->rows = rows;
+        this->cols = cols;
+        color = new char*[rows];
+        F0R(i, rows) color[i] = new char[cols];
+        pref = new int*[rows];
+        F0R(i, rows) pref[i] = new int[cols];
+    }
+
+    void doCalculations() {
+        F0R(r, rows) F0R(c, cols) {
+            pref[r][c] = color[r][c] != '.';
+            if(c > 0)
+                pref[r][c] += pref[r][c - 1];
+            else if(r > 0)
+                pref[r][c] += pref[r - 1][cols - 1];
+            if(color[r][c] != '.')
+                bottomRight = MP(r, c);
+        }
+        F0R(r, n) F0R(c, m) {
+            if(r < rows && c < cols && color[r][c] != '.')
+                hash = hash + (powPrime[r * m + c] * (color[r][c] - 'a' + 1));
+        }
+    }
+
+    void print() {
+        F0R(r, rows) {
+            F0R(c, cols) cout << color[r][c];
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+};
+
+struct FigureGroup {
+
+    Figure figs[8];
+
+    FigureGroup(Figure f = Figure()) {
+        figs[0] = f;
+        figs[1] = flip(figs[0]);
+        FOR(i, 2, 8) figs[i] = rot(figs[i - 2]);
+        F0R(i, 8) figs[i].doCalculations();
+    }
+
+    Figure rot(Figure a) {
+        Figure res(a.cols, a.rows);
+        F0R(i, a.rows) F0R(j, a.cols) res.color[j][a.rows - i - 1] = a.color[i][j];
+        return res;
+    }
+
+    Figure flip(Figure a) {
+        Figure res(a.rows, a.cols);
+        F0R(i, a.rows) F0R(j, a.cols) res.color[i][a.cols - j - 1] = a.color[i][j];
+        return res;
+    }
+
+};
 
 ifstream fin("bcs.in");
 ofstream fout("bcs.out");
+
 int k;
-fig cow;
-fig figure[MAXK][8];
-pii q[MAXN * MAXN];
-unordered_map<ll, vi> withHash;
-bool res[MAXK][MAXK][MAXK], withArea[MAXN * MAXN + 1];
+bool tripleWorks[MAXK][MAXK][MAXK];
 
-void tryAdding(int a, int b, int i) {
-    if(figure[a][0].A + figure[b][0].A + figure[i][0].A != cow.A) return;
-    if(i < min(a, b)) res[i][min(a, b)][max(a, b)] = 1;
-    else if(i > max(a, b)) res[min(a, b)][max(a, b)][i] = 1;
-    else res[min(a, b)][i][max(a, b)] = 1;
+map<pll, vi> mp;
+
+Figure cow;
+FigureGroup pieces[MAXK];
+
+int area(Figure f) {
+    return f.pref[f.rows - 1][f.cols - 1];
 }
 
-void figuresWork(fig a, fig b, int aID, int bID) {
-    int ptr = 0;
-    bool got[cow.F.S][cow.F.F[0].size()];
-    F0R(row, cow.F.S) F0R(col, cow.F.F[0].size()) {
-        if(cow.F.F[row][col] != '.') q[ptr++] = MP(row, col), got[row][col] = 0;
-        else got[row][col] = 1;
-    }
-    ptr = 0;
-    pii topLeft = q[ptr];
-    F0R(row, a.F.S) F0R(col, a.F.F[0].size()) {
-        int cowR = topLeft.F + row - a.firstSpot.F;
-        int cowC = topLeft.S + col - a.firstSpot.S;
-        if(a.F.F[row][col] == '.') continue;
-        if(cowR < 0 || cowR >= cow.F.S || cowC < 0 || cowC >= cow.F.F[0].size() || got[cowR][cowC] || cow.F.F[cowR][cowC] != a.F.F[row][col]) return;
-        got[cowR][cowC] = true;
-        if(got[q[ptr].F][q[ptr].S]) ptr++;
-    }
-    topLeft = q[ptr];
-    F0R(row, b.F.S) F0R(col, b.F.F[0].size()) {
-        int cowR = topLeft.F + row - b.firstSpot.F;
-        int cowC = topLeft.S + col - b.firstSpot.S;
-        if(b.F.F[row][col] == '.') continue;
-        if(cowR < 0 || cowR >= cow.F.S || cowC < 0 || cowC >= cow.F.F[0].size() || got[cowR][cowC] || cow.F.F[cowR][cowC] != b.F.F[row][col]) return;
-        got[cowR][cowC] = true;
-    }
-    int minR = 10000, maxR = 0, minC = 10000, maxC = 0;
-    F0R(row, cow.F.S) F0R(col, cow.F.F[0].size()) {
-        if(!got[row][col]) {
-            minR = min(minR, row), maxR = max(maxR, row);
-            minC = min(minC, col), maxC = max(maxC, col);
-        }
-    }
-    ll h = 0;
-    FOR(row, minR, maxR + 1) {
-        FOR(col, minC, maxC + 1) {
-            h *= 379;
-            if(!got[row][col]) h += cow.F.F[row][col] - 'a' + 1;
-        }
-        h *= 379;
-        h += 100;
-    }
-    if(!withHash.count(h)) return;
-    for(const int i : withHash[h]) if(i != aID && i != bID) tryAdding(aID, bID, i);
+void addAnswer(int a, int b, int c) {
+    tripleWorks[min(a, min(b, c))][max(a,b) ^ max(a, c) ^ max(b, c)][max(a, max(b, c))] = 1;
 }
 
-void tryDoing(int a, int b) {
-    if(figure[a][0].A + figure[b][0].A >= cow.A || !withArea[cow.A - figure[a][0].A - figure[b][0].A]) return;
-    F0R(rotA, 8) F0R(rotB, 8) figuresWork(figure[a][rotA], figure[b][rotB], a, b);
+void addWith(pll h, int a, int b) {
+    if(mp.count(h))
+        for(const int c : mp[h])
+            addAnswer(a, b, c);
 }
 
-fig readFigure() {
+int getPref(Figure f, pii offset, int r, int c) {
+    r -= offset.F, c -= offset.S;
+    if(r >= f.rows)
+        return area(f);
+    c = min(c, f.cols - 1);
+    if(c < 0)
+        r--, c = f.cols - 1;
+    if(r >= 0)
+        return f.pref[r][c];
+    return 0;
+}
+
+pii calcBottomRight(Figure f, pii offset) {
+    int cutoff = area(cow) - area(f);
+    int lo = 0, hi = n * m - 1;
+    while(lo < hi) {
+        int mid = (lo + hi) >> 1;
+        if(cow.pref[mid / m][mid % m] - getPref(f, offset, mid / m, mid % m) == cutoff)
+            hi = mid;
+        else
+            lo = mid + 1;
+    }
+    return MP(lo / m, lo % m);
+}
+
+void work(Figure aFig, Figure bFig, int a, int b) {
+    pii aOffset = cow.bottomRight - aFig.bottomRight;
+    if(aOffset.F < 0 || aOffset.F + aFig.rows > n || aOffset.S < 0 || aOffset.S + aFig.cols > m)
+        return;
+    pll h = cow.hash - (aFig.hash * powPrime[m * aOffset.F + aOffset.S]);
+    pii bOffset = calcBottomRight(aFig, aOffset) - bFig.bottomRight;
+    if(bOffset.F < 0 || bOffset.F + bFig.rows > n || bOffset.S < 0 || bOffset.S + bFig.cols > m)
+        return;
+    h = h - (bFig.hash * powPrime[m * bOffset.F + bOffset.S]);
+    addWith(h, a, b);
+}
+
+void test(int a, int b) {
+    cout << a << " " << b << endl;
+    if(area(pieces[a].figs[0]) + area(pieces[b].figs[0]) >= area(cow))
+        return;
+    F0R(aRot, 8) F0R(bRot, 8) work(pieces[a].figs[aRot], pieces[b].figs[bRot], a, b);
+    F0R(aRot, 8) F0R(bRot, 8) work(pieces[b].figs[bRot], pieces[a].figs[aRot], b, a);
+}
+
+Figure readFig() {
     int r, c;
     fin >> r >> c;
-    string *res = new string[r];
-    int area = 0;
-    pii fs;
-    F0R(row, r) {
-        fin >> res[row];
-        F0R(col, c) {
-            if(res[row][col] != '.') {
-                area++;
-                if(area == 1) fs = MP(row, col);
-            }
-        }
+    char **color = new char*[r];
+    F0R(i, r) color[i] = new char[c];
+    F0R(i, r) F0R(j, c) fin >> color[i][j];
+    int minR = INT_MAX, maxR = 0, minC = INT_MAX, maxC = 0;
+    F0R(i, r) F0R(j, c) if(color[i][j] != '.') {
+        minR = min(i, minR), maxR = max(i, maxR);
+        minC = min(j, minC), maxC = max(j, maxC);
     }
-    return MP(MP(res, r), MP(area, fs));
+    Figure res(maxR - minR + 1, maxC - minC + 1);
+    F0R(i, maxR - minR + 1) F0R(j, maxC - minC + 1) res.color[i][j] = color[minR + i][minC + j];
+    return res;
 }
 
-fig rotate90(fig a) {
-    string *res = new string[a.F.F[0].size()];
-    int area = 0;
-    pii fs;
-    F0R(row, a.F.F[0].size()) {
-        res[row] = "";
-        F0R(col, a.F.S) {
-            res[row] += a.F.F[a.F.S - 1 - col][row];
-            if(res[row][col] != '.') {
-                area++;
-                if(area == 1) fs = MP(row, col);
-            }
-        }
-    }
-    return MP(MP(res, a.F.F[0].size()), MP(area, fs));
-}
-
-fig flipHoriz(fig a) {
-    string *res = new string[a.F.S];
-    int area = 0;
-    pii fs;
-    F0R(row, a.F.S) {
-        res[row] = "";
-        F0R(col, a.F.F[0].size()) {
-            res[row] += a.F.F[row][a.F.F[0].size() - 1 - col];
-            if(res[row][col] != '.') {
-                area++;
-                if(area == 1) fs = MP(row, col);
-            }
-        }
-    }
-    return MP(MP(res, a.F.S), MP(area, fs));
-}
-
-ll getHash(fig a) {
-    ll h = 0;
-    F0R(row, a.F.S) {
-        F0R(col, a.F.F[0].size()) {
-            h *= 379;
-            if(a.F.F[row][col] != '.') h += a.F.F[row][col] - 'a' + 1;
-        }
-        h *= 379;
-        h += 100;
-    }
-    return h;
+void print(pll p) {
+    cout << "(" << p.F << ", " << p.S << ")" << endl;
 }
 
 int main() {
     fin >> k;
-    cow = readFigure();
-    F0R(i, k) {
-        figure[i][0] = readFigure();
-        withArea[figure[i][0].A] = 1;
-        FOR(j, 1, 4) figure[i][j] = rotate90(figure[i][j - 1]);
-        figure[i][4] = flipHoriz(figure[i][0]);
-        FOR(j, 5, 8) figure[i][j] = rotate90(figure[i][j - 1]);
-        unordered_set<ll> s;
-        F0R(j, 8) {
-            ll h = getHash(figure[i][j]);
-            if(s.count(h)) continue;
-            s.insert(h);
-            if(!withHash.count(h)) withHash.insert(MP(h, vi()));
-            withHash[h].PB(i);
+    cow = readFig();
+    n = cow.rows, m = cow.cols;
+    powPrime[0] = MP(1, 1);
+    FOR(i, 1, n * m + 1) powPrime[i] = prime * powPrime[i - 1];
+    cow.doCalculations();
+    F0R(i, k) pieces[i] = FigureGroup(readFig());
+    F0R(i, k) F0R(j, 8) {
+        cout << i << " " << j << endl;
+        Figure f = pieces[i].figs[j];
+        F0R(rShift, n - f.rows + 1) F0R(cShift, m - f.cols + 1) {
+            int po = rShift * m + cShift;
+            pll h = f.hash * powPrime[po];
+            if(!mp.count(h))
+                mp.insert(MP(h, vi()));
+            if(mp[h].empty() || mp[h].back() != i)
+                mp[h].PB(i);
         }
     }
-    F0R(a, k) FOR(b, a + 1, k) tryDoing(a, b), tryDoing(b, a);
-    int total = 0;
-    F0R(a, k) FOR(b, a + 1, k) FOR(c, b + 1, k) if(res[a][b][c]) total++;
-    fout << total << "\n";
+    F0R(a, k) FOR(b, a + 1, k) test(a, b);
+    int res = 0;
+    F0R(a, k) FOR(b, a + 1, k) FOR(c, b + 1, k) if(tripleWorks[a][b][c])
+        res++;
+    fout << res << endl;
     return 0;
 }
