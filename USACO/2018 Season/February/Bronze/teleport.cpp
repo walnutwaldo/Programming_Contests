8bbc28551295db9e178c8d68260c489fce642da5
Walden Yan <walnutwaldo20@gmail.com>
2018-03-18 00:37:23 -0400

    Create teleport.cpp

---
+++ USACO/2018 Season/February/Bronze/teleport.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define SQ(a) (a)*(a)
+
+#define F0R(i, a) for(int i = 0; i < (a); i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define R0F(i, a) for(int i = (a) - 1; i >= 0; i--)
+#define ROF(i, a, b) for(int i = (b) - 1; i >= (a); i--)
+
+#define ran() (rand() & 0x7FFF)
+#define rand31() ((ran() << 16) | (ran() << 1) | (ran() & 1))
+#define rand32() ((ran() << 17) | (ran() << 2) | (ran() & 3))
+#define rand63() (((ll)ran() << 48) | ((ll)ran() << 33) | ((ll)ran() << 18) | ((ll)ran() << 3) | ((ll)ran() & 7))
+#define rand64() (((ll)ran() << 49) | ((ll)ran() << 34) | ((ll)ran() << 19) | ((ll)ran() << 4) | ((ll)ran() & 15))
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MT make_tuple
+#define UB upper_bound
+#define LB lower_bound
+#define X real()
+#define Y imag()
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef complex<ld> point;
+
+int a, b, x, y;
+
+int main() {
+    ifstream fin("teleport.in");
+    ofstream fout("teleport.out");
+    fin >> a >> b >> x >> y;
+    fout << min(abs(a - b), min(abs(a - x) + abs(b - y), abs(b - x) + abs(a - y)));
+    return 0;
+}
