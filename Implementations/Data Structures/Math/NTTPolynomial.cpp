f20c1d9a0adeae4e295b3a50e25d54769811bb17
Walden Yan <walnutwaldo20@gmail.com>
2018-04-09 22:24:37 -0400

    Update NTTPolynomial.cpp

--- Implementations/Data Structures/Math/NTTPolynomial.cpp
+++ Implementations/Data Structures/Math/NTTPolynomial.cpp
@@ -2,7 +2,6 @@
 // https://github.com/walnutwaldo/Programming_Contests/blob/master/Implementations/Algorithms/NTT.cpp
 
 using namespace NTT;
-ull tempPVPoly[1 << MAX_DEG];
 
 struct Polynomial {
 
@@ -13,7 +12,6 @@ struct Polynomial {
         deg = d;
         coefficients = new ull[deg + 1];
         memset(coefficients, 0, (deg + 1) * sizeof(ull));
-        buildRT();
     }
 
     Polynomial operator ()(Polynomial p) {
@@ -27,27 +25,25 @@ struct Polynomial {
         return res;
     }
 
-    ull& operator [](int idx) { return coefficients[idx]; }
-
     Polynomial operator +(ull a) {
         Polynomial p(deg);
         F0R(i, deg + 1) p[i] = coefficients[i];
         p[0] += a;
-        p[0] %= NTTMod;
+        p[0] %= NTT_MOD;
         return p;
     }
 
     Polynomial operator -(ull a) {
         Polynomial p(deg);
         F0R(i, deg + 1) p[i] = coefficients[i];
-        p[0] += NTTMod - a;
-        p[0] %= NTTMod;
+        p[0] += NTT_MOD - a;
+        p[0] %= NTT_MOD;
         return p;
     }
 
     Polynomial operator *(ull mult) {
         Polynomial p(deg);
-        F0R(i, deg + 1) p[i] = coefficients[i] * mult % NTTMod;
+        F0R(i, deg + 1) p[i] = coefficients[i] * mult % NTT_MOD;
         return p;
     }
 
@@ -55,29 +51,27 @@ struct Polynomial {
 
     Polynomial operator +(Polynomial b) {
         Polynomial res(max(deg, b.deg));
-        F0R(i, max(deg, b.deg) + 1) res[i] = (((i > deg) ? 0 : coefficients[i]) + ((i > b.deg) ? 0 : b[i])) % NTTMod;
+        F0R(i, max(deg, b.deg) + 1) res[i] = (((i > deg) ? 0 : coefficients[i]) + ((i > b.deg) ? 0 : b[i])) % NTT_MOD;
         return res;
     }
 
-    Polynomial operator -(Polynomial p) { return *this + (p * (NTTMod - 1)); }
+    Polynomial operator -(Polynomial p) { return *this + (p * (NTT_MOD - 1)); }
 
     Polynomial operator *(Polynomial b) {
-        int neededDeg = 32 - __builtin_clz(deg + b.deg);
-        ntt(neededDeg, coefficients, deg + 1);
-        F0R(i, (1 << neededDeg)) tempPVPoly[i] = NTTBuild[i][0];
-        ntt(neededDeg, b.coefficients, b.deg + 1);
-        F0R(i, (1 << neededDeg)) tempPVPoly[i] = tempPVPoly[i] * NTTBuild[i][0] % NTTMod;
-        invntt(neededDeg, tempPVPoly, 1 << neededDeg);
-        Polynomial res(deg + b.deg);
-        F0R(i, res.deg + 1) res[i] = NTTBuild[i][0];
+        vul va, vb;
+        F0R(i, deg + 1) va.PB((*this)[i]);
+        F0R(i, b.deg + 1) vb.PB(b[i]);
+        vul vc = NTT::conv(va, vb);
+        Polynomial res(vc.size() - 1);
+        F0R(i, vc.size()) res[i] = vc[i];
         return res;
     }
 
-    void operator +=(ull a) { coefficients[0] += a, coefficients[0] %= NTTMod; }
+    void operator +=(ull a) { coefficients[0] += a, coefficients[0] %= NTT_MOD; }
 
-    void operator -=(ull a) { *this += NTTMod - a; }
+    void operator -=(ull a) { *this += NTT_MOD - a; }
 
-    void operator *=(ull mult) { F0R(i, deg + 1) coefficients[i] *= mult, coefficients[i] %= NTTMod; }
+    void operator *=(ull mult) { F0R(i, deg + 1) coefficients[i] *= mult, coefficients[i] %= NTT_MOD; }
 
     void operator /=(ull div) { *this *= invMod(div); }
 
@@ -86,5 +80,7 @@ struct Polynomial {
     void operator -=(Polynomial p) { *this = *this - p; }
 
     void operator *=(Polynomial b) { *this = *this * b; }
+
+    ull& operator [](int idx) { return coefficients[idx]; }
 
 };
