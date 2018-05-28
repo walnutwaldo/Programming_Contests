766bc32cb48c34e1f3d66d318d78169062f849bb
Walden Yan <walnutwaldo20@gmail.com>
2018-05-27 23:51:40 -0400

    Create lemonade.cpp

---
+++ USACO/2018 Season/US Open/Silver/lemonade.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+#define SQ(a) (a)*(a)
+
+#define F0R(i, a) for(int i = 0; i < (a); i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define R0F(i, a) for(int i = (a) - 1; i >= 0; i--)
+#define ROF(i, a, b) for(int i = (b) - 1; i >= (a); i--)
+
+#define ran() (rand() & 0x7FFF)
+#define rand31() ((ran() << 16) | (ran() << 1) | (ran() & 1))
+#define rand32() ((ran() << 17) | (ran() << 2) | (ran() & 3))
+#define rand63() (((ll)ran() << 48) | ((ll)ran() << 33) | ((ll)ran() << 18) | ((ll)ran() << 3) | ((ll)ran() & 7))
+#define rand64() (((ll)ran() << 49) | ((ll)ran() << 34) | ((ll)ran() << 19) | ((ll)ran() << 4) | ((ll)ran() & 15))
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MT make_tuple
+#define UB upper_bound
+#define LB lower_bound
+#define X real()
+#define Y imag()
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ull> vul;
+typedef complex<ld> point;
+
+int n;
+vi v;
+
+int main() {
+	ifstream fin("lemonade.in");
+	ofstream fout("lemonade.out");
+	fin >> n;
+	F0R(i, n) {
+		int w;
+		fin >> w;
+		v.PB(w);
+	}
+	sort(v.rbegin(), v.rend());
+	int res = n;
+	F0R(i, n) if(v[i] < i) {
+		res = i;
+		break;
+	}
+	fout << res << endl;
+}
