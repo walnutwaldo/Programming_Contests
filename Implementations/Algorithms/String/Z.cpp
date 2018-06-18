cc211d6ba8a44ee85c5ffc4f202706d58713913f
Walden Yan <walnutwaldo20@gmail.com>
2018-06-18 16:54:48 -0400

    Update Z.cpp

--- Implementations/Algorithms/String/Z.cpp
+++ Implementations/Algorithms/String/Z.cpp
@@ -1,8 +1,8 @@
 vi z(string s) {
-  vi res(sz(s));
+  vi z(sz(s));
   int lo = 0, hi = 0;
   FOR(i, 1, sz(s)) {
-    res[i] = max(0, min(hi - i + 1, res[i - lo]));
+    z[i] = max(0, min(hi - i + 1, z[i - lo]));
     while(i + z[i] < sz(s) && s[i + z[i]] == s[z[i]]) lo = i, hi = i + z[i]++;
   }
   return res;
