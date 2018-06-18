372234e6c37a43f1363f69e94d5fb36fa7f9e72b
Walden Yan <walnutwaldo20@gmail.com>
2018-06-18 16:43:03 -0400

    Create Z.cpp

---
+++ Implementations/Algorithms/String/Z.cpp
@@ -0,0 +1,11 @@
+vi z(string s) {
+  vi res(sz(s));
+  int lo = 0, hi = 0;
+  FOR(i, 1, sz(s)) {
+    res[i] = max(0, min(hi - i + 1, res[i - lo]));
+    while(i + z[i] < sz(s) && s[i + z[i]] == s[z[i]]) {
+      lo = i, hi = i + z[i]++;
+    }
+  }
+  return res;
+}
