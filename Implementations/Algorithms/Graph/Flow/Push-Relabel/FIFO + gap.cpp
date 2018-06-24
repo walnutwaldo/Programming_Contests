eafbe091e467e5f8b7e166022e61f8b4e64038cd
Walden Yan <walnutwaldo20@gmail.com>
2018-06-23 22:49:43 -0400

    Update FIFO + gap.cpp

--- Implementations/Algorithms/Graph/Flow/Push-Relabel/FIFO + gap.cpp
+++ Implementations/Algorithms/Graph/Flow/Push-Relabel/FIFO + gap.cpp
@@ -1,62 +1,50 @@
-int n, m, h[MAXN], curr[MAXN], cnt[MAXN + 1];
+int n, m, maxH, h[MAXN], cnt[MAXN];
 ll e[MAXN], c[MAXN][MAXN], f[MAXN][MAXN];
 vi neigh[MAXN];
-queue<int> overflows;
+vector<pair<int, ll>> adj[MAXN];
+deque<int> q;
 
 ll cf(int u, int v) { return c[u][v] - f[u][v]; }
 
-void initPreflow(int s, int t) {
-	h[s] = n;
-	F0R(i, n) if(c[s][i]) {
-		f[s][i] = c[s][i]; f[i][s] = -c[s][i];
-		e[i] += c[s][i]; e[s] -= c[s][i];
-		overflows.push(i);
-	}
-}
-
-void push(int u, int v) {
-	int flow = min(e[u], cf(u, v));
-	f[u][v] += flow; f[v][u] -= flow;
-	e[u] -= flow; e[v] += flow;
-	if(e[v] == flow) overflows.push(v);
-}
-
-void relabel(int node) {
-	int mn = INT_MAX;
-	cnt[h[node]]--;
-	F0R(i, n) if(cf(node, i) > 0) mn = min(mn, h[i]);
-	h[node] = min(n, mn + 1);
-	cnt[h[node]]++;
+void push(int u, int v, int t) {
+	ll flow = min(e[u], cf(u, v));
+	f[u][v] += flow, f[v][u] -= flow;
+	e[u] -= flow, e[v] += flow;
+	if(v != t && e[v] == flow) q.PB(v);
 }
 
 void gap(int k) {
-	F0R(i, n) if(h[i] >= k) {
+	F0R(i, n) if(h[i] < n && h[i] >= k) {
 		cnt[h[i]]--;
 		h[i] = n;
-		cnt[h[i]]++;
 	}
 }
 
-void discharge(int u) {
-	while(e[u] && h[u] < n) {
-		int v = curr[u];
-		if(v == sz(neigh[u])){
-			if(cnt[h[u]] == 1) gap(h[u]);
-			else relabel(u);
-			curr[u] = 0;
-		} else if (cf(u, neigh[u][v]) && h[u] == h[neigh[u][v]] + 1) push(u, neigh[u][v]);
-		else curr[u]++;
-	}
+void relabel(int u) {
+	cnt[h[u]]--;
+	h[u] = n;
+	for(const int v : neigh[u]) if(cf(u, v) > 0) h[u] = min(h[u], h[v] + 1);
+	if(h[u] < n) cnt[h[u]]++, q.push_front(u);
+}
+
+void discharge(int u, int t) {
+	for(const int v : neigh[u])
+		if(e[u] && cf(u, v) && h[v] + 1 == h[u]) push(u, v, t);
+		else if(!e[u]) break;
+	if(e[u] && cnt[h[u]] == 1) gap(h[u]);
+	else if(e[u]) relabel(u);
 }
 
 ll flow(int s, int t) {
-	initPreflow(s, t);
-	F0R(i, n) F0R(j, n) if(c[i][j] || c[j][i]) neigh[i].PB(j);
-	F0R(i, n) cnt[h[i]]++;
-	while(!overflows.empty()) {
-		int node = overflows.front(); overflows.pop();
-		if(node == s || node == t || h[node] == n) continue;
-		discharge(node);
+	F0R(i, n) for(const pair<int, ll> p : adj[i]) {
+		if(!c[i][p.F] && !c[p.F][i]) neigh[i].PB(p.F), neigh[p.F].PB(i);
+		c[i][p.F] += p.S;
+	}
+	for(const pair<int, ll> p : adj[s]) e[s] += p.S; 
+	cnt[0] = n; q.PB(s);
+	while(!q.empty()) {
+		int u = q.front(); q.pop_front();
+		if(h[u] < n) discharge(u, t);
 	}
 	return e[t];
 }
