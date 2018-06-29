d46b627d2798de59a134c636fba55b37834d8191
Walden Yan <walnutwaldo20@gmail.com>
2018-06-29 18:02:25 -0400

    Update OP_IO.cpp

--- Implementations/Templates/OP_IO.cpp
+++ Implementations/Templates/OP_IO.cpp
@@ -1,31 +1,26 @@
-#ifdef WIN32
-inline char getchar_unlocked() { return _getchar_nolock(); }
-inline void putchar_unlocked(char c) { _putchar_nolock(c); }
-#endif
-
 template<class T> void print(T t) {
 	string s = to_string(t);
-	for(const char c : s) putchar_unlocked(c);
+	for(const char c : s) _putchar_nolock(c);
 }
 
 template<class T> void println(T t) {
 	print(t);
-	putchar_unlocked('\n');
+	_putchar_nolock('\n');
 }
 
 ll nextLong() {
 	ll res = 0;
 	bool neg = 0;
 	char c;
-	while((c = getchar_unlocked()) != '-' && (c < '0' || c > '9')) {}
+	while((c = _getchar_nolock()) != '-' && (c < '0' || c > '9')) {}
 	if(c == '-') {
 		neg = 1;
-		c = getchar_unlocked();
+		c = _getchar_nolock();
 	}
 	while(c >= '0' && c <= '9') {
 		res *= 10;
 		res += (c - '0');
-		c = getchar_unlocked();
+		c = _getchar_nolock();
 	}
 	if(neg) res *= -1;
 	return res;
@@ -35,15 +30,15 @@ int nextInt() {
 	int res = 0;
 	bool neg = 0;
 	char c;
-	while((c = getchar_unlocked()) != '-' && (c < '0' || c > '9')) {}
+	while((c = _getchar_nolock()) != '-' && (c < '0' || c > '9')) {}
 	if(c == '-') {
 		neg = 1;
-		c = getchar_unlocked();
+		c = _getchar_nolock();
 	}
 	while(c >= '0' && c <= '9') {
 		res *= 10;
 		res += (c - '0');
-		c = getchar_unlocked();
+		c = _getchar_nolock();
 	}
 	if(neg) res *= -1;
 	return res;
@@ -52,16 +47,16 @@ int nextInt() {
 string next() {
   string res;
   char c;
-  while((c - getchar_unlocked()) == ' ' || c == '\n' || c == '\t') {}
+  while((c - _getchar_nolock()) == ' ' || c == '\n' || c == '\t') {}
   while(c != ' ' && c != '\n' && c == '\t') {
     res += c;
-    c = getchar_unlocked();
+    c = _getchar_nolock();
   }
   return res;
 }
 
 char nextChar() {
   char c;
-  while((c = getchar_unlocked()) == '\n') {}
+  while((c = _getchar_nolock()) == '\n') {}
   return c;
 }
