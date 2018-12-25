43d22d4548b2683e948b6a3df3cce0dc6cf52de5
Walden Yan <walnutwaldo20@gmail.com>
2018-12-25 15:32:52 -0500

    Create balance.cpp

---
+++ USACO/2018-2019 Season/December/Platinum/balance.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+#define F0R(i, a) for(int i = 0; i < (a); i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define R0F(i, a) for(int i = (a) - 1; i >= 0; i--)
+#define ROF(i, a, b) for(int i = (b) - 1; i >= (a); i--)
+
+#define ran() (rand() & 0x7FFF)
+#define rand31() ((ran() << 16) | (ran() << 1) | (ran() & 1))
+#define rand32() ((ran() << 17) | (ran() << 2) | (ran() & 3))
+#define rand63() (((ll)ran() << 48) | ((ll)ran() << 33) | ((ll)ran() << 18) | ((ll)ran() << 3) | ((ll)ran() & 7))
+#define rand64() (((ll)ran() << 49) | ((ll)ran() << 34) | ((ll)ran() << 19) | ((ll)ran() << 4) | ((ll)ran() & 15))
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MT make_tuple
+#define UB upper_bound
+#define LB lower_bound
+#define X real()
+#define Y imag()
+
+#define PI acos(-1)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define SQ(x) ((x) * (x))
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<ll> vll;
+typedef vector<ull> vul;
+typedef complex<ld> point;
+typedef complex<ld> cld;
+typedef vector<cld> vcld;
+
+#define MAXN 100000
+
+int n;
+ld p[MAXN + 2];
+
+int main() {
+    ifstream fin("balance.in");
+    ofstream fout("balance.out");
+    fin >> n;
+    FOR(i, 1, n + 1) {
+        fin >> p[i];
+        p[i] *= 100000;
+    }
+    vi v;
+    F0R(i, n + 2) {
+        while(sz(v) >= 2 && (p[i] - p[v[sz(v) - 2]]) / (i - v[sz(v) - 2]) < (p[i] - p[v.back()]) / (i - v.back())) v.pop_back();
+        v.PB(i);
+    }
+    FOR(i, 1, n + 1) {
+        int lb = v[UB(all(v), i) - v.begin() - 1];
+        int ub = v[UB(all(v), i) - v.begin()];
+        ld res = (p[ub] * (i - lb) + p[lb] * (ub - i)) / (ub - lb);
+        fout << (ll)res << "\n";
+    }
+}
