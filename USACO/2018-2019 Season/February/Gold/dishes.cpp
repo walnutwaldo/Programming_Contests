a20b0dd1fb0720e4e3d7ed1419429192e0fd1845
Walden Yan <walnutwaldo20@gmail.com>
2019-03-05 17:43:30 -0500

    Update dishes.cpp

--- USACO/2018-2019 Season/February/Gold/dishes.cpp
+++ USACO/2018-2019 Season/February/Gold/dishes.cpp
@@ -3,7 +3,6 @@
 #define F0R(i, a) for (int i = 0; i < a; i++)
 
 #define PB push_back
-#define LB lower_bound
 
 using namespace std;
 
@@ -11,46 +10,28 @@ typedef vector<int> vi;
 
 #define MAXN 100000
 
-int n, a[MAXN];
-vector<vi> cntr;
-vi cntrtop;
-int ptr;
-
-bool possible(int pref) {
-	vi v;
-	F0R(i, pref) v.PB(a[i]);
-	sort(v.rbegin(), v.rend());
-	
-	ptr = 0;
-	cntr.clear();
-	cntrtop.clear();
-	F0R(i, pref) {
-		int insidx = ptr;
-		if(ptr < (int)cntrtop.size() && a[i] > cntrtop[ptr]) insidx = LB(cntrtop.begin() + ptr + 1, cntrtop.end(), a[i]) - cntrtop.begin();
-		if(insidx == (int)cntrtop.size()) cntr.PB(vi()), cntrtop.PB(0);
-		cntr[insidx].PB(a[i]);
-		cntrtop[insidx] = a[i];
-		while(!v.empty() && ptr < (int)cntr.size() && cntr[ptr].back() == v.back()) {
-			v.pop_back();
-			cntr[ptr].pop_back();
-			if(cntr[ptr].empty()) ptr++;
-			else cntrtop[ptr] = cntr[ptr].back();
-		}
-	}
-	return v.empty();
-}
+int n, mx, base[MAXN + 1];
+vi cntr[MAXN + 1];
 
 int main() {
 	ifstream cin("dishes.in");
 	ofstream cout("dishes.out");
 	cin >> n;
-	F0R(i, n) cin >> a[i];
-	int lo = 0, hi = n;
-	while(lo < hi) {
-		int mid = (lo + hi + 1) >> 1;
-		if(possible(mid)) lo = mid;
-		else hi = mid - 1;
+	int mx = 0;
+	F0R(i, n) {
+		int a;
+		cin >> a;
+		if(a < mx) {
+			cout << i << endl;
+			exit(0);
+		}
+		for(int j = a; j && !base[j]; j--) base[j] = a;
+		while(!cntr[base[a]].empty() && cntr[base[a]].back() < a) {
+			mx = max(mx, cntr[base[a]].back());
+			cntr[base[a]].pop_back();
+		}
+		cntr[base[a]].PB(a);
 	}
-	cout << lo << endl;
+	cout << n << endl;
 	return 0;
 }
