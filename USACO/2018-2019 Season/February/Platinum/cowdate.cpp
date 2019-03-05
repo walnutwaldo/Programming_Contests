e4b3c1dd597d8b72b831a5f6134d4a24fca04493
Walden Yan <walnutwaldo20@gmail.com>
2019-03-05 16:13:59 -0500

    Create cowdate.cpp

---
+++ USACO/2018-2019 Season/February/Platinum/cowdate.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define R0F(i, a) for (int i = (a) - 1; i >= 0; i--)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); i--)
+
+#define ran() (rand() & 0x7FFF)
+#define rand31() ((ran() << 16) | (ran() << 1) | (ran() & 1))
+#define rand32() ((ran() << 17) | (ran() << 2) | (ran() & 3))
+#define rand63() (((ll)ran() << 48) | ((ll)ran() << 33) | ((ll)ran() << 18) | ((ll)ran() << 3) | ((ll)ran() & 7))
+#define rand64() (((ll)ran() << 49) | ((ll)ran() << 34) | ((ll)ran() << 19) | ((ll)ran() << 4) | ((ll)ran() & 15))
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MT make_tuple
+#define UB upper_bound
+#define LB lower_bound
+#define X real()
+#define Y imag()
+
+#define INF 1e18
+#define PI acos(-1)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define SQ(x) ((x) * (x))
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<ll> vll;
+typedef vector<ull> vul;
+typedef complex<ld> point;
+typedef complex<ld> cld;
+typedef vector<cld> vcld;
+
+#define MAXN 1000000
+
+int n;
+ld p[MAXN];
+
+int main() {
+    ifstream cin("cowdate.in");
+    ofstream cout("cowdate.out");
+    cin >> n;
+    F0R(i, n) {
+        ll p_in;
+        cin >> p_in;
+        p[i] = (ld)p_in / 1000000;
+    }
+    ld res = 0, s = 0, prod = 1;
+    int lo = 0, hi = 0;
+    while (lo < n) {
+        while (hi < n && s < 1) {
+            s += p[hi] / (1 - p[hi]);
+            prod *= 1 - p[hi];
+            hi++;
+        }
+        res = max(res, s * prod);
+        s -= p[lo] / (1 - p[lo]);
+        prod /= 1 - p[lo];
+        lo++;
+    }
+    cout << (ll)floor(1000000 * res) << endl;
+}
