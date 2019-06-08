c018c98cd47cb2168d0186b8cfd9c3e6f10aa932
Walden Yan <walnutwaldo20@gmail.com>
2019-06-07 21:42:10 -0400

    Update SegmentTree.cpp

--- Implementations/Data Structures/Range Queries/Segment Trees/SegmentTree.cpp
+++ Implementations/Data Structures/Range Queries/Segment Trees/SegmentTree.cpp
@@ -1,12 +1,12 @@
-template<typedef T>
+template<typename T>
 struct SegmentTree {
 
     function<T(T, T)> comb;
     int sz;
     T *tree;
 
-    SegmentTree(int _sz, function<T(T, T)> _comb) {
-        sz = 1 << (32 - __builtin_clz(_sz - 1));
+    SegmentTree(int _sz = 0, function<T(T, T)> _comb = nullptr) {
+		sz = 1 << (32 - __builtin_clz(_sz - 1));
 		comb = _comb;
         tree = new T[sz << 1];
         memset(tree, 0, 2 * sz * sizeof(T));
@@ -20,7 +20,7 @@ struct SegmentTree {
 
     T query(int lo, int hi) { // both ends includsive
         lo += sz, hi += sz;
-        T total = tree[lo];
+        T total = tree[lo++];
         while(lo <= hi) {
             if(lo % 2 == 1) total = comb(total, tree[lo++]);
             if(hi % 2 == 0) total = comb(total, tree[hi--]);
