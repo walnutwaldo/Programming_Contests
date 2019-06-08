63aae959764e234398206d470eaa7031a82d3227
Walden Yan <walnutwaldo20@gmail.com>
2019-06-07 21:17:55 -0400

    testing new seg tree

--- Implementations/Data Structures/Range Queries/Segment Trees/SegmentTree.cpp
+++ Implementations/Data Structures/Range Queries/Segment Trees/SegmentTree.cpp
@@ -1,25 +1,26 @@
+template<typedef T>
 struct SegmentTree {
 
+	function<T(T, T)> comb;
     int sz;
-    ll *tree;
+    T *tree;
 
-    SegmentTree(int pre_sz) {
-        sz = 1 << (int)ceil(log2(pre_sz));
-        tree = new ll[2 * sz];
-        memset(tree, 0, 2 * sz * sizeof(ll));
+    SegmentTree(int _sz, function<T(T, T)> _comb) {
+        sz = 1 << (32 - __builtin_clz(_sz - 1));
+		comb = _comb;
+        tree = new T[sz << 1];
+        memset(tree, 0, 2 * sz * sizeof(T));
     }
     
-    ll comb(ll a, ll b) { return a + b; } // modify this
-
-    void update(int idx, ll v) { // sets element at index idx to v
+    void update(int idx, T v) { // sets element at index idx to v
         idx += sz;
         tree[idx] = v;
         for(idx /= 2; idx > 0; idx /= 2) tree[idx] = comb(tree[2 * idx], tree[2 * idx + 1]);
     }
 
-    ll query(int lo, int hi) { // both ends includsive
+    T query(int lo, int hi) { // both ends includsive
         lo += sz, hi += sz;
-        ll total = tree[lo];
+        T total = tree[lo];
         while(lo <= hi) {
             if(lo % 2 == 1) total = comb(total, tree[lo++]);
             if(hi % 2 == 0) total = comb(total, tree[hi--]);
