33bdb3b23e38607550291023ee9c54918de34a40
Walden Yan <walnutwaldo20@gmail.com>
2019-06-07 21:03:59 -0400

    Update SegmentTree.cpp

--- Implementations/Data Structures/Range Queries/Segment Trees/SegmentTree.cpp
+++ Implementations/Data Structures/Range Queries/Segment Trees/SegmentTree.cpp
@@ -8,20 +8,21 @@ struct SegmentTree {
         tree = new ll[2 * sz];
         memset(tree, 0, 2 * sz * sizeof(ll));
     }
+    
+    ll comb(ll a, ll b) { return a + b; } // modify this
 
-    void update(int idx, ll v) {
+    void update(int idx, ll v) { // sets element at index idx to v
         idx += sz;
-        tree[idx] += v;
-        for(idx /= 2; idx > 0; idx /= 2) tree[idx] = tree[2 * idx] + tree[2 * idx + 1];
+        tree[idx] = v;
+        for(idx /= 2; idx > 0; idx /= 2) tree[idx] = comb(tree[2 * idx], tree[2 * idx + 1]);
     }
 
-    ll query(int lo, int hi) {
-        lo += sz;
-        hi += sz;
-        ll total = 0;
+    ll query(int lo, int hi) { // both ends includsive
+        lo += sz, hi += sz;
+        ll total = tree[lo];
         while(lo <= hi) {
-            if(lo % 2 == 1) total += tree[lo++];
-            if(hi % 2 == 0) total += tree[hi--];
+            if(lo % 2 == 1) total = comb(total, tree[lo++]);
+            if(hi % 2 == 0) total = comb(total, tree[hi--]);
             lo /= 2;
             hi /= 2;
         }
